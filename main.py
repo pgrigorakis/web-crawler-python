77f35b2b30b5462d338fa308b957e8607732ea06
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-07-01 15:14:39 +0200

    Added functionality to get user input

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import sys
+
+
 def main():
-    print("Hello from web-crawler-python!")
+    args = sys.argv
+
+    if len(args) < 2:
+        print("no website provided")
+        exit(1)
+
+    if len(args) > 2:
+        print("too many arguments provided")
+        exit(1)
+
+    base_url = args[1]
+    print(f"starting crawl of: {base_url}...")
 
 
 if __name__ == "__main__":
