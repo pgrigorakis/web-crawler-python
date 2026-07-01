fe81c5d30ebb64df47020f2c1361a74007e286ae
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-07-01 19:57:12 +0200

    Added async crawling functionality

---
+++ async_crawler.py
@@ -0,0 +1,96 @@
+import asyncio
+from urllib.parse import urlparse
+from types import TracebackType
+
+import aiohttp
+
+from crawl import PageData, extract_page_data, normalize_url
+
+
+async def crawl_site_async(base_url: str, max_concurrency: int = 1):
+    async with AsyncCrawler(base_url, max_concurrency) as crawler:
+        page_data = await crawler.crawl()
+
+    return page_data
+
+
+class AsyncCrawler:
+    def __init__(self, base_url: str, max_concurrency):
+        self.base_url = base_url
+        self.base_domain = urlparse(base_url).netloc
+        self.page_data = {}
+        self.lock = asyncio.Lock()
+        self.max_concurrency = max_concurrency
+        self.semaphore = asyncio.Semaphore(self.max_concurrency)
+        self.session: aiohttp.ClientSession | None = None
+
+    async def __aenter__(self):
+        self.session = aiohttp.ClientSession()
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        assert self.session is not None
+        await self.session.close()
+
+    async def add_page_visit(self, normalized_url: str) -> bool:
+        async with self.lock:
+            if normalized_url in self.page_data:
+                return False
+            return True
+
+    async def get_html(self, url):
+        try:
+            async with self.session.get(
+                url, headers={"User-Agent": "BootCrawler/1.0"}
+            ) as response:
+                if response.status > 399:
+                    print(f"Error: HTTP {response.status}: {response.reason}")
+                    return None
+
+                content_type = response.headers.get("Content-Type")
+                if "text/html" not in content_type:
+                    print(f"content type not text/html: {content_type}")
+                    return None
+
+                return await response.text()
+        except Exception as e:
+            print(f"network error while fetching {url}: {e}")
+            return None
+
+    async def crawl_page(self, current_url: str):
+        parsed_current_url = urlparse(current_url)
+
+        if self.base_domain != parsed_current_url.netloc:
+            return
+
+        norm_current_url = normalize_url(current_url)
+        if not await self.add_page_visit(norm_current_url):
+            return
+
+        try:
+            async with self.semaphore:
+                html_body = await self.get_html(current_url)
+                if html_body is None:
+                    return
+            extracted_html_elems = extract_page_data(html_body, current_url)
+        except Exception as e:
+            print(f"Failed to crawl {current_url}: {e}")
+            return
+
+        async with self.lock:
+            self.page_data[norm_current_url] = extracted_html_elems
+
+        tasks = [
+            asyncio.create_task(self.crawl_page(url))
+            for url in extracted_html_elems["outgoing_links"]
+        ]
+        await asyncio.gather(*tasks)
+
+    async def crawl(self) -> dict[str, PageData]:
+        await self.crawl_page(self.base_url)
+        return self.page_data
